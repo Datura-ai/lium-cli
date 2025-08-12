563683815b63c5ca4f7e100bf1c763a17b886c0a
Mikhail Derbichev <arhangel662@gmail.com>
2025-08-12 19:07:50 +0300

    feat: add Dind column to ls output

--- cli/commands/ls.py
+++ cli/commands/ls.py
@@ -107,6 +107,7 @@ def _specs_row(specs: Optional[Dict]) -> Dict[str, str]:
         "TFLOPs": _maybe_int(d.get("graphics_speed")),
         "NetUp": _maybe_int(net.get("upload_speed")),
         "NetDn": _maybe_int(net.get("download_speed")),
+        "Dind": "Yes" if specs.get("sysbox_runtime", False) else "No",
     }
 
 
@@ -145,6 +146,7 @@ def _add_long_columns(t: Table) -> None:
     t.add_column("TFLOPs",  justify="right", width=8, no_wrap=True)
     t.add_column("Net ↑",   justify="right", width=8, no_wrap=True)  # note the space
     t.add_column("Net ↓",   justify="right", width=8, no_wrap=True)
+    t.add_column("Dind",    justify="right", width=8, no_wrap=True)
 
     # absorb remaining width on the right with Location
     t.add_column("Location", justify="left", ratio=4, min_width=10, overflow="fold")
@@ -229,6 +231,7 @@ def show_executors(
             s["TFLOPs"],
             s["NetUp"],
             s["NetDn"],
+            s["Dind"],
             _country_name(exe.location),
         )
 
